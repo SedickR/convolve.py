d41af2eec0033f6c0ad445b6fc1b72269a9c7476
Emileta <emile.tremblay.antoine@gmail.com>
2022-05-05 12:41:09 -0400

    Un peu plus d'options

--- traitement.py
+++ traitement.py
@@ -16,46 +16,46 @@ class Simulation():
     def load_data(self, filename):
         '''Load data from a Zemax .txt file'''
         self.df = pd.read_csv(filename, sep=';')
-        self.matrix_s = np.resize(np.array(self.df.loc[:,'Spot Size']), (12,12))
-        self.matrix_r = np.resize(np.array(self.df.loc[:, 'R Spot Size']), (12,12))
-        self.matrix_g = np.resize(np.array(self.df.loc[:, 'G Spot Size']), (12,12))
-        self.matrix_b = np.resize(np.array(self.df.loc[:, 'B Spot Size']), (12,12))
+        self.matrix_s = (np.resize(np.array(self.df.loc[:,'Spot Size']), (12,12)), 'Combined')
+        self.matrix_r = (np.resize(np.array(self.df.loc[:, 'R Spot Size']), (12,12)), 'Red')
+        self.matrix_g = (np.resize(np.array(self.df.loc[:, 'G Spot Size']), (12,12)), 'Green')
+        self.matrix_b = (np.resize(np.array(self.df.loc[:, 'B Spot Size']), (12,12)), 'Blue')
     
-    def plot_maker(self, matrix):
+    def heatmap_maker(self, matrix):
         '''Create figure from matrix'''
-        fig = go.Figure(data=go.Heatmap(z=matrix))
+        fig = go.Figure(data=go.Heatmap(z=matrix[0]))
         fig.update_yaxes(title_text='Row index')
         fig.update_xaxes(title_text='Column index')
-        return fig
+        return fig, matrix[1]
 
 
     def plot_matrix(self, save=False, rgb=False, r=False, g=False, b=False):
         '''Plot the Spot Size matrix'''
         plots = []
-        plots.append(self.plot_maker(self.matrix_s))
+        plots.append(self.heatmap_maker(self.matrix_s))
         if rgb:
-            plots.append(self.plot_maker(self.matrix_r))
-            plots.append(self.plot_maker(self.matrix_g))    
-            plots.append(self.plot_maker(self.matrix_b))
+            plots.append(self.heatmap_maker(self.matrix_r))
+            plots.append(self.heatmap_maker(self.matrix_g))    
+            plots.append(self.heatmap_maker(self.matrix_b))
         if r:
-            plots.append(self.plot_maker(self.matrix_r))
+            plots.append(self.heatmap_maker(self.matrix_r))
         if g:
-            plots.append(self.plot_maker(self.matrix_g))
+            plots.append(self.heatmap_maker(self.matrix_g))
         if b:
-            plots.append(self.plot_maker(self.matrix_b))        
+            plots.append(self.heatmap_maker(self.matrix_b))        
         if save:
-            for fig in plots:
-                fig.write_image(f'matrix_heatmap\\heatmap_{datetime.datetime.now().isoformat("_", "minutes").replace(":", "")}.pdf')
+            for fig, name in plots:
+                fig.write_image(f'matrix_heatmap\\{name}_heatmap_{datetime.datetime.now().isoformat("_", "minutes").replace(":", "")}.pdf')
                 time.sleep(0.5)
-                fig.write_image(f'matrix_heatmap\\heatmap_{datetime.datetime.now().isoformat("_", "minutes").replace(":", "")}.pdf')
+                fig.write_image(f'matrix_heatmap\\{name}_heatmap_{datetime.datetime.now().isoformat("_", "minutes").replace(":", "")}.pdf')
         else:
-            for fig in plots:
+            for fig, name in plots:
                 fig.show()
 
 
     def plot_diagonal(self, save=False):
         '''Plot the gradient of the Spot Size matrix'''
-        diag = np.flipud(self.matrix_s).diagonal()
+        diag = np.flipud(self.matrix_s[0]).diagonal()
         grad = np.gradient(diag)
         fig = go.Figure(data=go.Scatter(x=np.arange(len(grad)), y=grad, mode='lines'))
         fig.update_yaxes(title_text='Spot Size grandient')
@@ -69,6 +69,6 @@ class Simulation():
 
 i = Simulation()
 i.load_data('data\\test.txt')
-i.plot_matrix(rgb=True)
+i.plot_matrix()
 i.plot_diagonal()
 
