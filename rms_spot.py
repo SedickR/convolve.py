87a640e54a8c7b6d4fb8cdfdd2eafe17bd267193
Emileta <emile.tremblay.antoine@gmail.com>
2022-05-11 13:14:55 -0400

    Review

--- rms_spot.py
+++ rms_spot.py
@@ -1,50 +1,100 @@
-import cv2
+import cv2 as cv
 import numpy as np
 import os
+import yaml
 
 
-def center_of_mass(path):
-    '''Arguement: path to image
-    Returns : coordinates of center of mass, image in grayscale'''
-    # read image through command line
-    img = cv2.imread(f'{path}')
+class image_analysis:
+    def __init__(self, path):
+        if path.endswith('.bmp'):
+            self.directory = os.path.dirname(path)
+            self.image = cv.imread(path)
+        else:
+            self.directory = path
+        self.background = None
 
-    # convert the image to grayscale
-    gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    def background_subtraction(self, image, background):
+        '''Arguement: image, background
+        Return: subtracted image'''
+        return cv.subtract(image, background)
 
-    # convert the grayscale image to binary image
-    ret,thresh = cv2.threshold(gray_image,10,255,0)
+    def center_of_mass(self, im_path, channel="r", background_path=None):
+        '''Arguement: path to image, color channel, path to background image
+        Returns : coordinates of center of mass, image in grayscale, [pixel width, unit]'''
+        # get display data
+        with open("display_data.yaml", 'r') as f:
+            display = yaml.safe_load(f)
 
-    # create a meshgrid for coordinate calculation
-    x_m, y_m = np.meshgrid(
-            np.arange(0, thresh.shape[1]),
-            np.arange(0, thresh.shape[0]),
-            sparse=False,
-            indexing='xy'
-        )
+        # read image through command line
+        img = cv.imread(f'{im_path}')
 
-    x_c = int(np.sum(x_m * thresh) / np.sum(thresh))
-    y_c = int(np.sum(y_m * thresh) / np.sum(thresh))
+        cv.imshow("image", img)
+        cv.waitKey(0)
 
-    return (x_c, y_c), thresh
+        #remove background if background path is given
+        if background_path:
+            background = cv.imread(f'{background_path}')
+            img = self.background_subtraction(img, background)
+        
+        cv.imshow("image", img)
+        cv.waitKey(0)
+
+        # split image into channels
+        b, g, r = cv.split(img)
+        if channel is "b":
+            _,thresh = cv.threshold(b,15,255,0)
+            color = 'blue'
+        elif channel is "g":
+            _,thresh = cv.threshold(g,15,255,0)
+            color = 'green'
+        else:
+            _,thresh = cv.threshold(r,15,255,0)
+            color = 'red'
+
+        cv.imshow("image", thresh)
+        cv.waitKey(0)
+
+        # define center of image
+        center = (int(thresh.shape[1]//2), int(thresh.shape[0]//2))
+
+        # mask unwanted pixels based on display measurements
+        cv.circle(thresh, (center[0]-display[color], center[1]+display[color]), display['noise_px_diameter'], (0,0,0), -1)
+        cv.circle(thresh, (center[0]+display[color], center[1]+display[color]), display['noise_px_diameter'], (0,0,0), -1)
+        cv.circle(thresh, (center[0]-display[color], center[1]-display[color]), display['noise_px_diameter'], (0,0,0), -1)
+        cv.circle(thresh, (center[0]+display[color], center[1]-display[color]), display['noise_px_diameter'], (0,0,0), -1)
+
+        cv.imshow("image", thresh)
+        cv.waitKey(0)
+
+        # create a meshgrid for coordinate calculation
+        x_m, y_m = np.meshgrid(
+                np.arange(0, thresh.shape[1]),
+                np.arange(0, thresh.shape[0]),
+                sparse=False,
+                indexing='xy'
+            )
+
+        # compute center of mass
+        x_c = int(np.sum(x_m * thresh) / np.sum(thresh))
+        y_c = int(np.sum(y_m * thresh) / np.sum(thresh))
 
 
-def rms_spot(center, img_gray):
-    '''Arguement: center of mass, grayscale image
-    Returns : rms spot size'''
-    # Find all non-zero pixels in the image
-    non_zero = cv2.findNonZero(img_gray)
-
-    # Distance of all non-zero pixels from the center of mass
-    dist = np.sqrt((non_zero[:,:,0] - center[0])**2 + (non_zero[:,:,1] - center[1])**2)
-    
-    # Return the rms spot size
-    return np.sqrt(np.mean(dist**2))
+        return (x_c, y_c), thresh, display['pixel_diameter']
 
 
+    def rms_spot(self, center, img_gray, units):
+        '''Arguement: center of mass, grayscale image, [pixel width, unit]
+        Returns : rms spot size, units'''
+        # Find all non-zero pixels in the image
+        non_zero = cv.findNonZero(img_gray)
+
+        # Distance of all non-zero pixels from the center of mass
+        dist = np.sqrt((non_zero[:,:,0] - center[0])**2 + (non_zero[:,:,1] - center[1])**2)
+
+        # Return the rms spot size
+        return np.sqrt(np.mean(dist**2))*units[0], units[1]
 
 
-for filename in os.listdir('images'):
-    cv2.imshow('image', center_of_mass(f'images\\{filename}')[1])
-    cv2.waitKey(0)
+i = image_analysis('red_spot_size_xrot\\background_spm.bmp')
 
+print(i.rms_spot(*i.center_of_mass('red_spot_size_xrot\\2h_x-1,5081d_y0d.bmp', 'r', 'red_spot_size_xrot\\background_spm.bmp')))
--- traitement.py
+++ traitement.py
@@ -5,86 +5,147 @@ import time
 import datetime
 import os
 
-class Simulation():
-    '''Zemax optical simulation data analysis and plotting'''
+#todo 0.75217, pas de rotation cas idéal
+#centre à centre deux pixel dans la matrice diagonale, 39.5 um
+
+class Simulation:
+    """Zemax optical simulation data analysis and plotting"""
+
     def __init__(self):
         self.df = None
         self.matrix_s = None
         self.matrix_r = None
         self.matrix_g = None
         self.matrix_b = None
-    
+
     def load_data(self, filename):
-        '''Load data from a Zemax .txt file'''
-        self.df = pd.read_csv(filename, sep=';')
-        self.matrix_s = (np.resize(np.array(self.df.loc[:,'Spot Size']), (12,12)), 'Combined')
-        self.matrix_r = (np.resize(np.array(self.df.loc[:, 'R Spot Size']), (12,12)), 'Red')
-        self.matrix_g = (np.resize(np.array(self.df.loc[:, 'G Spot Size']), (12,12)), 'Green')
-        self.matrix_b = (np.resize(np.array(self.df.loc[:, 'B Spot Size']), (12,12)), 'Blue')
-    
+        """Load data from a Zemax .txt file"""
+        self.df = pd.read_csv(filename, sep=";")
+        self.matrix_s = (
+            np.resize(np.array(self.df.loc[:, "Spot Size"]), (12, 12)),
+            "Combined",
+        )
+        self.matrix_r = (
+            np.resize(np.array(self.df.loc[:, "R Spot Size"]), (12, 12)),
+            "Red",
+        )
+        self.matrix_g = (
+            np.resize(np.array(self.df.loc[:, "G Spot Size"]), (12, 12)),
+            "Green",
+        )
+        self.matrix_b = (
+            np.resize(np.array(self.df.loc[:, "B Spot Size"]), (12, 12)),
+            "Blue",
+        )
+
     def heatmap_maker(self, matrix):
-        '''Create figure from matrix'''
-        fig = go.Figure(data=go.Heatmap(z=matrix[0], colorbar={"title":'Spot Size [μm]'}))
-        fig.update_yaxes(title_text='Row index')
-        fig.update_xaxes(title_text='Column index')
-        fig.update_layout(title=f'Spot Size RSRH')
+        """Create figure from matrix"""
+        fig = go.Figure(
+            data=go.Heatmap(z=matrix[0], colorbar={"title": "Spot Size [μm]"})
+        )
+        fig.update_yaxes(title_text="Row index")
+        fig.update_xaxes(title_text="Column index")
+        fig.update_layout(title=f"Spot Size RSRH")
         return fig, matrix[1]
 
-
     def plot_matrix(self, save=False, rgb=False, r=False, g=False, b=False):
-        '''Plot the Spot Size matrix'''
+        """Plot the Spot Size matrix"""
         plots = []
         plots.append(self.heatmap_maker(self.matrix_r))
         if rgb:
             plots.append(self.heatmap_maker(self.matrix_r))
-            plots.append(self.heatmap_maker(self.matrix_g))    
+            plots.append(self.heatmap_maker(self.matrix_g))
             plots.append(self.heatmap_maker(self.matrix_b))
         if r:
             plots.append(self.heatmap_maker(self.matrix_r))
         if g:
             plots.append(self.heatmap_maker(self.matrix_g))
         if b:
-            plots.append(self.heatmap_maker(self.matrix_b))      
+            plots.append(self.heatmap_maker(self.matrix_b))
         if save:
             for fig, name in plots:
-                fig.write_image(f'matrix_heatmap\\{name}_heatmap_{datetime.datetime.now().isoformat("_", "minutes").replace(":", "")}.pdf')
+                fig.write_image(
+                    f'matrix_heatmap\\{name}_heatmap_{datetime.datetime.now().isoformat("_", "minutes").replace(":", "")}.pdf'
+                )
                 time.sleep(0.5)
-                fig.write_image(f'matrix_heatmap\\{name}_heatmap_{datetime.datetime.now().isoformat("_", "minutes").replace(":", "")}.pdf')
+                fig.write_image(
+                    f'matrix_heatmap\\{name}_heatmap_{datetime.datetime.now().isoformat("_", "minutes").replace(":", "")}.pdf'
+                )
         else:
             for fig, name in plots:
                 fig.show()
 
-
     def plot_diagonal(self, save=False):
-        '''Plot the gradient of the Spot Size matrix'''
+        """Plot the gradient of the Spot Size matrix"""
         matrix = self.matrix_r[0]
         diag = matrix.diagonal()
         grad = np.gradient(diag)
-        fig = go.Figure(data=go.Scatter(x=np.arange(len(grad)), y=grad, mode='lines', name='diagonal'))
+        fig = go.Figure(
+            data=go.Scatter(
+                x=np.arange(len(grad)),
+                y=grad,
+                mode="lines",
+                name="diagonal",
+                line=dict(shape="linear", color="black"),
+            )
+        )
         grad_v = np.gradient(matrix[:, 0])
-        fig.add_trace(go.Scatter(x = np.arange(len(grad_v)), y = grad_v, mode='lines', name = 'vertical'))
+        fig.add_trace(
+            go.Scatter(
+                x=np.arange(len(grad_v)),
+                y=grad_v,
+                mode="lines",
+                name="vertical",
+                line=dict(shape="linear", color="black", dash="dot"),
+            )
+        )
         grad_h = np.gradient(matrix[0, :])
-        fig.add_trace(go.Scatter(x = np.arange(len(grad_h)), y = grad_h, mode='lines', name = 'horizontal'))
-        fig.update_yaxes(title_text='Spot Size grandient')
-        fig.update_xaxes(title_text='Distance from center')
-        fig.update_layout(title=f'Spot Size Gradient RSRH')
+        fig.add_trace(
+            go.Scatter(
+                x=np.arange(len(grad_h)),
+                y=grad_h,
+                mode="lines",
+                name="horizontal",
+                line=dict(shape="linear", color="black", dash="dot"),
+            )
+        )
+        fig.add_trace(
+            go.Scatter(x=np.arange(len(matrix[:, 0])), y=matrix[:, 0], mode="lines")
+        )
+        fig.update_yaxes(title_text="Spot Size grandient")
+        fig.update_xaxes(title_text="Distance from center")
+        fig.update_layout(title=f"Spot Size Gradient RSRH")
         if save:
-            fig.write_image(f'diagonal_gradient\\gradient_{datetime.datetime.now().isoformat("_", "minutes").replace(":", "")}.pdf')
+            fig.write_image(
+                f'diagonal_gradient\\gradient_{datetime.datetime.now().isoformat("_", "minutes").replace(":", "")}.pdf'
+            )
             time.sleep(1)
-            fig.write_image(f'diagonal_gradient\\gradient_{datetime.datetime.now().isoformat("_", "minutes").replace(":", "")}.pdf')
+            fig.write_image(
+                f'diagonal_gradient\\gradient_{datetime.datetime.now().isoformat("_", "minutes").replace(":", "")}.pdf'
+            )
         else:
             fig.show()
 
-    def plot_all_files(self, directory, matrix=True, diagonal=True, save=False, rgb=False, r=False, g=False, b=False):
-        '''Plot all text file in the directory'''
+    def plot_all_files(
+        self,
+        directory,
+        matrix=True,
+        diagonal=True,
+        save=False,
+        rgb=False,
+        r=False,
+        g=False,
+        b=False,
+    ):
+        """Plot all text file in the directory"""
         for file in os.listdir(directory):
-            if file.endswith('H.txt'):
-                self.load_data(directory+'\\'+file)
+            if file.endswith("E.txt"):
+                self.load_data(directory + "\\" + file)
                 if matrix:
                     self.plot_matrix(save=save, rgb=rgb, r=r, g=g, b=b)
                 if diagonal:
                     self.plot_diagonal(save=save)
 
-i = Simulation()
-i.plot_all_files('data_batch', matrix=False)
 
+i = Simulation()
+i.plot_all_files("data_batch")
