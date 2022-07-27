bb9e0f8eb78a9aaf4feca73a2ae6d4791e8c5de8
SedickR <89620039+SedickR@users.noreply.github.com>
2022-07-27 15:39:18 -0400

    deconvolution2

--- convolve.py
+++ convolve.py
@@ -1,24 +1,38 @@
 import numpy as np
 from scipy.signal import fftconvolve
-import cv2
+import scipy.misc
+from scipy import ndimage
+#import cv2
+from PIL import Image
 import matplotlib.pyplot as plt
 from tkinter.filedialog import askopenfile
 
 #load psf and image
+object_path = askopenfile()
 psf_path = askopenfile()
-image_path = askopenfile()
-psf = np.loadtxt(psf_path, skiprows=30, delimiter='\t')
-image = np.loadtxt(image_path, skiprows=17, delimiter='\t')
-
+image_zemax_path = askopenfile()
+object = np.loadtxt(object_path, skiprows=17, delimiter='\t')
+image_output = Image.fromarray(object)
+psf = np.loadtxt(psf_path, skiprows=19, delimiter='\t')
+image_zemax = np.loadtxt(image_zemax_path, skiprows=17, delimiter='\t')
 #convolve image with psf
-convolved = fftconvolve(image, psf, mode='same')
+convolved = fftconvolve(object, psf, mode='same')
 
-#plot image and convolved image
+
+
 plt.figure(figsize=(10,10))
-plt.subplot(1,2,1)
-plt.imshow(image, cmap='gray')
+plt.subplot(1,4,1)
+plt.imshow(object, cmap='gray')
 plt.title('Image')
-plt.subplot(1,2,2)
+plt.subplot(1,4,2)
+plt.imshow(psf, cmap='gray')
+plt.title('PSF')
+plt.subplot(1,4,3)
 plt.imshow(convolved, cmap='gray')
 plt.title('Convolved Image')
+plt.subplot(1,4,4)
+img = image_zemax
+rotated_img = ndimage.rotate(img, 180)
+plt.imshow(rotated_img, cmap='gray')
+plt.title('Zemax Simulation')
 plt.show()
